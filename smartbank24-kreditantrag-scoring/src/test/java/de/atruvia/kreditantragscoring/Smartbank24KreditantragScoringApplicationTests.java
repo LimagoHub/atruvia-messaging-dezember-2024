484e15151dbebdc6f58a2a5ab068e3d39737662c
Jo Wagner <jowagner@limago.de>
2024-12-11 09:58:49 +0100

    uebung scoring

---
+++ smartbank24-kreditantrag-scoring/src/test/java/de/atruvia/kreditantragscoring/Smartbank24KreditantragScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.atruvia.kreditantragscoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smartbank24KreditantragScoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
