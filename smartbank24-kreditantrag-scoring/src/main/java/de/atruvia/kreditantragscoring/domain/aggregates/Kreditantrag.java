fca49419cb5dabaab6bf9e5d6fac79e7c2bc1f1a
Jo Wagner <jowagner@limago.de>
2024-12-11 14:02:24 +0100

    pom

---
+++ smartbank24-kreditantrag-scoring/src/main/java/de/atruvia/kreditantragscoring/domain/aggregates/Kreditantrag.java
@@ -0,0 +1,21 @@
+package de.atruvia.kreditantragscoring.domain.aggregates;
+
+import lombok.*;
+
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@Builder
+public class Kreditantrag {
+
+    private String creditApplicationId ;
+    private double monthlyIncome;
+    private double monthlyExpenditure;
+    private double creditSum;
+
+    public boolean isKreditwuerdig() {
+        return creditSum < (monthlyIncome-monthlyExpenditure) * 10.0;
+    }
+}
